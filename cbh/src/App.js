28f69a5175a9e41ca15592d15f56f245ab4c9c0c
riri6969 <riri.zamor@gmail.com>
2020-04-09 15:45:47 +0200

    #US0 test

---
+++ cbh/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Cards Battlle of Heroes</h1>
+
+    </div>
+  );
+}
+
+export default App;
