60a3c8b5393da9f857965e8aa5c24c7f962a11ee
riri6969 <riri.zamor@gmail.com>
2020-04-15 21:50:29 +0200

    #8 Navbar add

--- src/components/NavBarDeckChoice.js
+++ src/components/NavBarDeckChoice.js
@@ -1,9 +1,21 @@
 import React from 'react';
+import './NavBarDekChoice.css'
+import { Link } from 'react-router-dom';
 
 function NavBarDeckChoice({ heroes }) {
     return (
         <div>
-
+            <nav>
+                <ul class="container" >
+                    <li>
+                        <Link to="/"> <button id='button-home' >&lt; Home</button> </Link>
+                    </li>
+                    <li id='title'>Choose your Heroes</li>
+                    <li>
+                        <button id='button-battle' link='/Battle' linkName='< Battle' >Let's Battle</button>
+                    </li>
+                </ul>
+            </nav>
         </div>
     )
 }
