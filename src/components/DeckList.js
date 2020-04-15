77336c3095e5db921a049421e8eff5308b055d61
riri6969 <riri.zamor@gmail.com>
2020-04-15 22:28:09 +0200

    #8 add limitation power on function SumPower

--- src/components/DeckList.js
+++ src/components/DeckList.js
@@ -3,15 +3,20 @@ import CardListChosen from './CardListChosen';
 import './DeckList.css';
 
 function DeckList(props) {
+    const PowerMax = 300;
 
     function SumPower() {
+        
         let totalPower = 0;
         const powerArray = props.heroesChosen.map(heroe => heroe.power);
         for (let i = 0; i < powerArray.length; i = i + 1) {
             totalPower += powerArray[i]
         }
-        
-        return totalPower;
+        if (totalPower > PowerMax) {
+            return "Please lower your deck power"
+        } else {
+            return totalPower;
+        }
     }
 
     return (
@@ -23,7 +28,7 @@ function DeckList(props) {
                     {props.heroesChosen.map(heroe => <CardListChosen heroechoice={heroe} />)}
                 </div>
             </div>
-            <button type="text" className="pw_button" id="pw_btn"> Power: {SumPower()} / 300 </button>
+            <button type="text" className="pw_button" id="pw_btn"> Power: {SumPower()} / {PowerMax} </button>
         </div>
     )
 
