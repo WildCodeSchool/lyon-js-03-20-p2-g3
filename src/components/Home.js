ca6b903d4c5c6e2f6a660d7d1bcbc952a3e3e75a
Astarosa <baptiste.shortgibet@gmail.com>
2020-04-15 11:09:15 +0200

    #5 css update for desktop media queries

--- src/components/Home.js
+++ src/components/Home.js
@@ -10,16 +10,15 @@ function Home () {
                 <h2>What kind of hero are you ?</h2>
                 <NavBar />
             </main>
-            <section id="social-media-container">  
+            <section id="social-media-container">
                 <img src={require('./images/iconfinder_square-facebook.png')} alt="saitama-ok"/> 
-                <a href="https://github.com/WildCodeSchool/lyon-js-03-20-p2-g3">
+                <a href="https://github.com/WildCodeSchool/lyon-js-03-20-p2-g3" target="_blank" rel="noopener noreferrer">
                     <img src={require('./images/iconfinder_github.png')} alt="saitama-ok"/>
                 </a>      
                 <img src={require('./images/iconfinder_square-twitter.png')} alt="saitama-ok"/>
             </section>
             <img id="saitama-home" src={require('./images/saitama-home.png')} alt="saitama-ok"/>
         </div>
-        
     );
 }
 
