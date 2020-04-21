e35cff6f056bbc000c0bd01df650c9cff6924332
Pascal Pereira <pereira.pascal@gmail.com>
2020-04-21 14:55:46 +0200

    #US8.2 deck, heroeschosen, deck'IA, hand in state

--- src/components/DeckBoard.js
+++ src/components/DeckBoard.js
@@ -17,17 +17,16 @@ class DeckBoard extends React.Component {
     // Looping on heroesChosen array to select the heroes not chosen  :
     const arrayOfAllHeroes = this.state.heroes;
     const arrayOfPlayerOneHeroes = this.state.heroesChosen;
-    const cardsAvalaibleForIA = this.extractItemsNotPresentArr1FromArr2(arrayOfPlayerOneHeroes,arrayOfAllHeroes);
+    const cardsAvalaibleForIA = this.extractItemsNotPresentArr1FromArr2(arrayOfPlayerOneHeroes, arrayOfAllHeroes);
     const threeFirstCards = this.randomizeHeroesChosen(arrayOfPlayerOneHeroes, 3);
     console.log(arrayOfPlayerOneHeroes);
     console.log(threeFirstCards);
-    
-    
-    const cardsInDeckPlayer1 = this.extractItemsNotPresentArr1FromArr2(threeFirstCards ,arrayOfPlayerOneHeroes)
+
+    const cardsInDeckPlayer1 = this.extractItemsNotPresentArr1FromArr2(threeFirstCards, arrayOfPlayerOneHeroes);
     // Update Player 1's hand cards
     this.setState({ player1Hand: threeFirstCards });
     // Update cards in the Player's Deck :
-    this.setState({player1Deck : cardsInDeckPlayer1})
+    this.setState({ player1Deck: cardsInDeckPlayer1 });
     // Update cards available for IA
     this.setState({ cardsAvalaibleForIA: cardsAvalaibleForIA });
   }
@@ -45,19 +44,19 @@ class DeckBoard extends React.Component {
     for (let i = 0; i < arrayOfHeroes.length; i++) {
       heroesChosenRandomized.push(arrayOfHeroes[arrayOfRandomNumbers[i]]);
     }
-    outputArray =  heroesChosenRandomized.slice(0, nbCardsToRandomize);
-    return outputArray
+    outputArray = heroesChosenRandomized.slice(0, nbCardsToRandomize);
+    return outputArray;
   }
 
-  extractItemsNotPresentArr1FromArr2  = (arr1,arr2)=> {
+  extractItemsNotPresentArr1FromArr2 = (arr1, arr2) => {
     // length of arr1 < length of arr2, all items in arr 1 are included in arr2
-    let outputArray=[];
+    const outputArray = [];
     for (let i = 0; i < arr2.length; i++) {
       if (arr1.indexOf(arr2[i]) === -1) {
         outputArray.push(arr2[i]);
       }
     }
-    return outputArray
+    return outputArray;
   }
 
   render () {
