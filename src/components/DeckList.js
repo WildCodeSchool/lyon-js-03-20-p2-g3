4797a1536e727380234d7eebd6f487d11ea11243
riri6969 <riri.zamor@gmail.com>
2020-04-23 09:36:21 +0200

    #9 add css decklist

--- src/components/DeckList.js
+++ src/components/DeckList.js
@@ -20,12 +20,13 @@ function DeckList (props) {
       <div className='deck_aside'>
         <div className='deck_title_container'>
           <h3 id='title_deck_chosen_cards'>Deck
-          
+
           </h3>
-          <span className="cross_title_container" onClick={props.removeDeck}>
-            <i className="fas fa-times"></i></span>
+          <span className='cross_title_container' onClick={props.removeDeck}>
+            <i className='fas fa-times' />
+          </span>
         </div>
-        
+
         <div className='cardlistdeck'>
           {props.heroesChosen.map(heroe => <CardListChosen key={heroe.name} heroechoice={heroe} addToDeck={props.addToDeck} />)}
 
