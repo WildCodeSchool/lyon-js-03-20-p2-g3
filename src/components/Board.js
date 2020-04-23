fca4126c3ed186462bb224a627e5cdb3be47ef27
florenthoudeille <houdeille.florent@hotmail.fr>
2020-04-23 10:15:38 +0200

    #28 add changes of Board.js

--- src/components/Board.js
+++ src/components/Board.js
@@ -6,13 +6,13 @@ import './Card.css';
 function Board (props) {
   /* const threeFirstCards = props.randomizeHeroesChosen(props.heroesChosen,3); */
   return (
-    <React.Fragment>
+    <>
       {props.heroesChosen.filter(heroe => heroe.position === 'board').map(heroe => {
         return (
           <CardOfDeckBoard heroe={heroe} key={(heroe.atk) * (heroe.hp)} />
         );
       })}
-    </React.Fragment>
+    </>
   );
 }
 export default Board;
