f7ff02f86c57cb2541e16ee3c92f2b7a040e6f56
Riri Z <riri.zamor@gmail.com>
2020-04-30 16:59:19 +0200

    Timer component fix semi-standar

---
+++ src/components/Timer.js
@@ -0,0 +1,53 @@
+import React, { Component } from 'react'
+
+class Timer extends Component {
+    constructor (props) {
+    super(props)
+    this.state = {
+        minutes: 0,
+        seconds: 60,
+    }
+    
+}
+
+    componentDidMount() {
+        this.myInterval = setInterval(() => {
+            const { seconds, minutes } = this.state
+
+            if (seconds > 0) {
+                this.setState(({ seconds }) => ({
+                    seconds: seconds - 1
+                }))
+            }
+            if (seconds === 0) {
+                if (minutes === 0) {
+                    clearInterval(this.myInterval)
+                } else {
+                    this.setState(({ minutes }) => ({
+                        minutes:0,
+                        seconds: 60
+                    }))
+                }
+            } 
+        }, 1000)
+    }
+
+    componentWillUnmount() {
+        clearInterval(this.myInterval)
+    }
+
+    render() {
+        const { minutes, seconds } = this.state
+        return (
+            <div>
+                { minutes === 0 && seconds === 0
+                    ? this.props.handleIaTurn()
+                    : <h1>Time Remaining: {minutes}:{seconds < 10 ? `0${seconds}` : seconds}</h1>
+                }
+            </div>
+        )
+    }
+
+}
+
+export default Timer;
