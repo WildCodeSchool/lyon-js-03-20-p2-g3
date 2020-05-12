b2ae81de718c144000b431ad2fd3a99d075c69ca
florenthoudeille <houdeille.florent@hotmail.fr>
2020-05-12 23:20:09 +0200

    fix CSS, thx baptiste

--- src/components/RulesCard.js
+++ src/components/RulesCard.js
@@ -5,7 +5,7 @@ import './Rules.css';
 function RulesCard (props) {
   return (
     <div className='singleCard'>
-      <div className='image'>
+      <div className='image-container'>
         <img src={props.image} alt={props.alt} />
       </div>
       <div className='explanation'>
